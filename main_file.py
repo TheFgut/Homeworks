373b31b899e61a422d29e2f8f303659b3c933d66
TheFgut <fgut.pikylia@gmail.com>
2022-02-13 11:44:21 +0200

    correction H7

--- main_file.py
+++
@@ -1,9 +0,0 @@
-from lib import check_allowance
-
-while True:
-    sentence = input("Write something: ")
-    print(sentence)
-
-    print("Do you want to try again?")
-    if check_allowance() is False:
-        break
